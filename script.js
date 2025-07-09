08b2b303343e60710e84d7cd1810a065e2578a43
Tejas Kulkarni <tejas.nk256@gmail.com>
2025-07-09 12:57:49 +0530

    Create script.js
    Third file for web hosting of project

---
+++ script.js
@@ -0,0 +1,18 @@
+// This is a single-line comment in JavaScript.
+// It explains the purpose of the following code.
+
+// The console.log() function is used to print output to the browser's console
+// or to the terminal if running with Node.js.
+console.log("Hello, World!");
+
+// You can also declare variables and perform operations.
+let message = "Welcome to JavaScript!";
+console.log(message);
+
+// Functions can be defined to encapsulate reusable blocks of code.
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// Call the function and log its return value.
+console
